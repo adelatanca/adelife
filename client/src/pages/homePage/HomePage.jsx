a97a6ad7a7ddd41a7257940a5595280d9eb1f721
Adela Tanca <adela.tanca@lateral-inc.com>
2023-04-04 22:52:58 +0300

    added navbar & login, register pages

---
+++ client/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import Navbar from 'pages/navbar/NavbarPage';
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default HomePage;
